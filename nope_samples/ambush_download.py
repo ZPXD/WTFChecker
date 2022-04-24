0d397899b9ef2c8bf30ab8d81109e754e344f5ea
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-04-24 11:22:15 +0200

    Basic structure sketch.

---
+++ nope_samples/ambush_download.py
@@ -0,0 +1,16 @@
+import request
+
+# Download by request
+
+
+import os
+
+# Download by os
+
+
+import subprocess
+
+# Download by subprocess
+
+
+# ...
---
+++ nope_samples/not_nice_command.py
@@ -0,0 +1,20 @@
+import os
+
+
+not_nice_1 = ''
+not_nice_2 = ''
+not_nice_3 = ''
+
+
+# Regular
+
+os.system(not_nice)
+
+
+# Loop
+
+for c in [not_nice_1, not_nice_2, not_nice_3]
+	os.system(c)
+
+# 
+
